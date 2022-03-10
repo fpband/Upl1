3da3f09aba6f41adaca2b00a10f079cc03fb808b
sahaynitin4tellyfun <87252200+sahaynitin4tellyfun@users.noreply.github.com>
2022-03-10 13:52:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Tellybots/Uploader-Bot-V2 /Uploader-Bot-V2
+git clone https://github.com/sahaynitin4tellyfun/Uploader-Bot-V2 /Uploader-Bot-V2
 cd /Uploader-Bot-V2
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
